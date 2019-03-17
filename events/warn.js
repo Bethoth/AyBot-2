046e783e0bab609bda9cf403d656c2176606e874
Ayfri <34159136+Ayfri@users.noreply.github.com>
2019-03-17 04:00:42 +0100

    Not used now.

--- events/warn.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-const moment = require("moment");
-module.exports = (client, warn) => {
-	console.log(chalk.red("Une petite erreur a été faite quelque part, \nheure : "+moment().format('MMMM Do YYY, hh:mm:ss a')+"\n\nErreur :"+warb));
-};
